20ca1e1e483229ee3ecab7123ae9550d7145e275
Roy Kincaid <roy@kincaid.ninja>
2024-12-11 16:14:45 +0100

    cleaned up user tab styling and layout. Added loading message

--- src/UserTab.tsx
+++ src/UserTab.tsx
@@ -2,8 +2,11 @@ import Container from "@cloudscape-design/components/container";
 import ChatBubble from "@cloudscape-design/chat-components/chat-bubble";
 // import ButtonGroup from "@cloudscape-design/components/button-group";
 // import StatusIndicator from "@cloudscape-design/components/status-indicator";
+import Header from "@cloudscape-design/components/header";
 import Avatar from "@cloudscape-design/chat-components/avatar";
 import PromptInput from "@cloudscape-design/components/prompt-input";
+import Box from "@cloudscape-design/components/box";
+import SpaceBetween from "@cloudscape-design/components/space-between";
 import * as React from "react";
 import { post } from 'aws-amplify/api';
 import { fetchAuthSession } from 'aws-amplify/auth';
@@ -16,6 +19,7 @@ export default function UserTab() {
     type: "incoming" | "outgoing";
     timestamp: Date;
     iconName: "gen-ai" | "user-profile";
+    color: "default" | "gen-ai";
     ariaLabel: string;
     tooltipText: string;
   };
@@ -29,13 +33,17 @@ export default function UserTab() {
       type: "incoming",
       timestamp: new Date(),
       iconName: "gen-ai",
+      color: "gen-ai",
       ariaLabel: "Generative AI assistant",
       tooltipText: "Generative AI assistant"
     }
   ]);
 
+  const [isLoading, setIsLoading] = React.useState(false);
+
   // Logic
   async function postItem(detail: string) {
+    setIsLoading(true);
     try {
 
       const session = await fetchAuthSession();
@@ -65,6 +73,7 @@ export default function UserTab() {
           type: "incoming",
           timestamp: new Date(),
           iconName: "gen-ai",
+          color: "gen-ai",
           ariaLabel: "Generative AI assistant",
           tooltipText: "Generative AI assistant",
         },
@@ -73,56 +82,80 @@ export default function UserTab() {
       console.log(response);
     } catch (error: any) {
       console.log('POST call failed: ', JSON.parse(error.response.body));
+    } finally {
+      setIsLoading(false);
     }
   }
 
   // Render
   return (
-    <Container header={<h2>Chat with a generative AI assistant</h2>}>
+      <SpaceBetween direction="vertical" size="m">
+      <Header variant="h1">Chat with a generative AI assistant</Header> 
+        <Container>
 
-      <Container>
-        {message.map((msg, index) => (
-          < ChatBubble
-            key={index}
-            ariaLabel={`${msg.sender} at ${msg.timestamp.toLocaleDateString()}`}
-            type={msg.type}
-            avatar={
-              <Avatar
-                iconName={msg.iconName}
-                ariaLabel={msg.ariaLabel}
-                tooltipText={msg.tooltipText}
-              />
-            }
-          >
-            {msg.content}
-          </ChatBubble>
-        ))}
-      </Container>
-
-      <PromptInput
-        onChange={({ detail }) => setValue(detail.value)}
-        value={value}
-        actionButtonAriaLabel="Send message"
-        actionButtonIconName="send"
-        ariaLabel="Prompt input with action button"
-        placeholder="Ask a question"
-        onAction={({ detail }) => {
-          postItem(detail.value);
-          setMessage((prevMessages) => [
-            ...prevMessages,
-            {
-              sender: "User",
-              content: detail.value,
-              type: "outgoing",
-              timestamp: new Date(),
-              iconName: "user-profile",
-              ariaLabel: "User",
-              tooltipText: "User",
-            },
-          ]);
-          setValue("");
-        }}
-      />
-    </Container>
+          {message.map((msg, index) => (
+            < ChatBubble
+              key={index}
+              ariaLabel={`${msg.sender} at ${msg.timestamp.toLocaleDateString()}`}
+              type={msg.type}
+              avatar={
+                <Avatar
+                  iconName={msg.iconName}
+                  color={msg.color}
+                  ariaLabel={msg.ariaLabel}
+                  tooltipText={msg.tooltipText}
+                />
+              }
+            >
+              {msg.content}
+            </ChatBubble>
+          ))}
+          {isLoading && (
+            <ChatBubble
+              ariaLabel="Generative AI assistant is thinking"
+              showLoadingBar
+              type="incoming"
+              avatar={
+                <Avatar
+                  loading={true}
+                  iconName="gen-ai"
+                  color="gen-ai"
+                  ariaLabel="Generative AI assistant"
+                  tooltipText="Generative AI assistant"
+                />
+              }
+            >
+              <Box color="text-status-inactive">
+                Generating response
+              </Box>
+            </ChatBubble>
+          )}
+        </Container>
+        <PromptInput
+          onChange={({ detail }) => setValue(detail.value)}
+          value={value}
+          actionButtonAriaLabel="Send message"
+          actionButtonIconName="send"
+          ariaLabel="Prompt input with action button"
+          placeholder="Ask a question"
+          onAction={({ detail }) => {
+            postItem(detail.value);
+            setMessage((prevMessages) => [
+              ...prevMessages,
+              {
+                sender: "User",
+                content: detail.value,
+                type: "outgoing",
+                timestamp: new Date(),
+                iconName: "user-profile",
+                color: "default",
+                ariaLabel: "User",
+                tooltipText: "User",
+              },
+            ]);
+            setValue("");
+          }}
+        />
+      </SpaceBetween>
   );
 }
