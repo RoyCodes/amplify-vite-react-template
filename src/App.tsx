62ddf1273e5f856304d456195123d41d2b915209
Roy Kincaid <roy@kincaid.ninja>
2024-12-11 19:01:06 +0100

    added grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "@cloudscape-design/global-styles/index.css";
 import "@aws-amplify/ui-react/styles.css";
 import Tabs from "@cloudscape-design/components/tabs";
 import ContentLayout from "@cloudscape-design/components/content-layout";
+import Grid from "@cloudscape-design/components/grid";
 import Container from "@cloudscape-design/components/container";
 import Button from "@cloudscape-design/components/button";
 import AdminTab from "./AdminTab"
@@ -13,30 +14,41 @@ function App() {
   return (
     <Authenticator>
       {({ signOut, user }) => (
-        <main>
-          <ContentLayout>
-            <Container header={<h1>Welcome, {user?.username || "User"}!</h1>}>
-              <Tabs
-                tabs={[
-                  {
-                    label: "Admin",
-                    id: "first",
-                    content: <AdminTab />
+        <Grid gridDefinition={[
+          {
+            colspan:
+            {
+              "default": 100,
+              xxs: 8
+            },
+            offset: 2
+          }]}
+        >
+          <main>
+            <ContentLayout>
+              <Container header={<h1>Welcome, {user?.username || "User"}!</h1>}>
+                <Tabs
+                  tabs={[
+                    {
+                      label: "Admin",
+                      id: "first",
+                      content: <AdminTab />
 
-                  },
-                  {
-                    label: "User",
-                    id: "second",
-                    content: <UserTab />
-                  }
-                ]}
-                variant="container"
-              />
-              <Button onClick={signOut}> Sign out </Button>
-            </Container>
-          </ContentLayout>
+                    },
+                    {
+                      label: "User",
+                      id: "second",
+                      content: <UserTab />
+                    }
+                  ]}
+                  variant="container"
+                />
+                <Button onClick={signOut}> Sign out </Button>
+              </Container>
+            </ContentLayout>
 
-        </main>
+          </main>
+        </Grid>
       )}
     </Authenticator>
   );
