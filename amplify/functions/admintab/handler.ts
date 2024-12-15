adb601f6ccdf2f2b2da606e9a7928f89a6d1d002
Roy Kincaid <roy@kincaid.ninja>
2024-12-15 06:27:25 -0800

    updated adminTab lambda to put raw doc uploads into processed docs

--- amplify/functions/admintab/handler.ts
+++ amplify/functions/admintab/handler.ts
@@ -1,6 +1,34 @@
 import type { S3Handler } from 'aws-lambda';
+import { S3Client, CopyObjectCommand } from '@aws-sdk/client-s3';
+
+const s3 = new S3Client({ region: process.env.AWS_REGION });
 
 export const handler: S3Handler = async (event) => {
-  const objectKeys = event.Records.map((record) => record.s3.object.key);
-  console.log(`Delete handler invoked for objects [${objectKeys.join(', ')}]`);
+
+  try {
+    for (const record of event.Records) {
+      const sourceBucket = record.s3.bucket.name;
+      const sourceKey = record.s3.object.key;
+      const fileExtension = sourceKey.split('.').pop()?.toLocaleLowerCase();
+      const audioExtensions = ['mp3', 'wave', 'flac'];
+
+      if (audioExtensions.includes(fileExtension || '')) {
+        console.log('audio file detected')
+        continue;
+      }
+
+      const destinationKey = `knowledge-base-processed-files/${sourceKey.split('/').pop()}`;
+
+      const copyCommand = new CopyObjectCommand({
+        Bucket: sourceBucket,
+        CopySource: `${sourceBucket}/${sourceKey}`,
+        Key: destinationKey,
+      });
+
+      await s3.send(copyCommand);
+    }
+  } catch (error) {
+    console.error('Error processing S3 event: ', error);
+    throw error;
+  }
 };
