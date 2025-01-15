8cd58a8d10b0f2a1133e35b8237ea8de79b69def
Roy Kincaid <roy@kincaid.ninja>
2025-01-14 18:34:40 -0800

    added css styling and visual tweaks to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,31 @@
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@cloudscape-design/global-styles/index.css";
 import "@aws-amplify/ui-react/styles.css";
+import "./index.css"
 import Tabs from "@cloudscape-design/components/tabs";
 import ContentLayout from "@cloudscape-design/components/content-layout";
-import Grid from "@cloudscape-design/components/grid";
 import Container from "@cloudscape-design/components/container";
 import Button from "@cloudscape-design/components/button";
+import SpaceBetween from "@cloudscape-design/components/space-between";
 import AdminTab from "./AdminTab"
 import UserTab from "./UserTab"
 
 function App() {
-
   return (
     <Authenticator>
       {({ signOut, user }) => (
-        <Grid gridDefinition={[
-          {
-            colspan:
-            {
-              "default": 100,
-              xxs: 8
-            },
-            offset: 2
-          }]}
-        >
-          <main>
-            <ContentLayout>
-              <Container header={<h1>Welcome, {user?.username || "User"}!</h1>}>
+        <main>
+          <ContentLayout>
+            <Container header={<h1>Welcome, {user?.username || "User"}!</h1>}>
+              <SpaceBetween size="l" direction="vertical">
                 <Tabs
                   tabs={[
                     {
                       label: "Admin",
                       id: "first",
-                      content: <AdminTab />
-
+                      content: (
+                        <AdminTab />
+                      ),
                     },
                     {
                       label: "User",
@@ -44,11 +36,10 @@ function App() {
                   variant="container"
                 />
                 <Button onClick={signOut}> Sign out </Button>
-              </Container>
-            </ContentLayout>
-
-          </main>
-        </Grid>
+              </SpaceBetween>
+            </Container>
+          </ContentLayout>
+        </main>
       )}
     </Authenticator>
   );
